2a30aa7cd2d10ab8a6a36ba2cc898ffff44ccc67
Leandro Pereira <leandronobper@gmail.com>
2024-12-05 16:16:25 -0300

    first commit

---
+++ front/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  public housingLocationList;
+  private housingService = new HousingService();
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
